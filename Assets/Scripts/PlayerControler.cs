6d6725747635db5e29d56132b7feb163bdadd960
matthias <matthiaspinard@gmail.com>
2018-01-24 15:59:02 +0100

    first commit

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour {
+	public Text score;
+	public Text winText;
+	private Rigidbody rb;
+	public float speed;
+	private int count;
+
+	void Start() 
+	{
+		rb = GetComponent<Rigidbody>();
+		count = 0;
+		UpdateScore();
+		winText.text = "";
+	}
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+		
+		rb.AddForce(movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Pick Up"))
+		{
+			other.gameObject.SetActive(false);
+			count++;
+			UpdateScore();
+			if(count == 7)
+			{
+				winText.text = "You win!";
+			}
+		}
+	}
+
+	void UpdateScore()
+	{
+		score.text = "Score : " + count;
+	}
+
+}
